146749391d91134dc94e898387ffc1060025cd12
tmth <tmth@tmth-pc>
2020-03-14 12:54:38 +0100

    Tooltip and dpt scaling

--- javascript/map.js
+++ javascript/map.js
@@ -1,5 +1,11 @@
 const width = 1000;
-const height = 500;
+const height = 600;
+
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
 
 var svg = d3.select("div#map")
   .classed("svg-container", true)
@@ -12,18 +18,17 @@ var svg = d3.select("div#map")
 const path = d3.geoPath();
 
 const projection = d3.geoConicConformal()
-  .center([2.14, 43.36])
-  .scale(5000);
+  .center([2.14, 43.9])
+  .scale(10000)
   // .translate([width / 2, height / 2]);
 
 path.projection(projection);
 
-
 const deps = svg.append("g");
 
-var div = d3.select("body").append("div")
-  .attr("class", "tooltip")
-  .style("opacity", 0);
+var div = d3.select("div#map").append("div")
+    .classed("tooltip", true)
+    .style("opacity", 0);
 
 d3.json('data/capacites.geojson').then(function(geojson) {
   deps.selectAll("path")
@@ -31,21 +36,53 @@ d3.json('data/capacites.geojson').then(function(geojson) {
     .enter()
     .append("path")
     .attr("d", path)
-    .attr("fill", "none")
+    .attr("fill", "white")
     .attr("stroke", "black")
     .attr("stroke-width", 3)
     .on("mouseover", function(d) {
+        var scale = 1.6;
+        var bb = this.getBBox();
+        console.log(bb);
+        d3.select(this)
+          .moveToFront()
+          .transition()
+          .duration(1000)
+          .attrTween("transform", function(d, i, a) {
+              return d3.interpolateString(a, 'scale('+scale+') translate('+((-10  -  170 * bb.x/500.)*scale/1.3)+', '+((-20 -  170 * bb.y/500.)*scale/1.3)+')')
+          })
+        div.transition()
+            .duration(200)
+            .style("opacity", .9);
+        div.html(d.properties.nom_dpt)
+            .style("left", getTooltipXposition(div))
+            .style("top", d3.event.pageY + "px");
+    })
+    .on('mousemove', function() {
       div.transition()
-        .duration(200)
-        .style("opacity", .9);
-      div.html(d.properties.nom_dpt)
-        .style("left", (d3.event.pageX + 30) + "px")
-        .style("top", (d3.event.pageY - 30) + "px")
+         .duration(0)
+         .style("opacity", .9);
+      div.style("left", getTooltipXposition(div))
+         .style("top", d3.event.pageY + "px");
     })
     .on("mouseout", function(d) {
-      div.style("opacity", 0);
-      div.html("")
-        .style("left", "-500px")
-        .style("top", "-500px");
+        d3.select(this)
+          .transition()
+          .duration(1000)
+          .attrTween("transform", function(d, i, a) {
+            return d3.interpolateString(a, 'scale(1)')
+          })
+        div.style("opacity", 0);
+        div.html("")
+            .style("left", "-500px")
+            .style("top", "-500px");
     });
 });
+
+function getTooltipXposition(div){
+  if (div.node().getBoundingClientRect().width+d3.event.pageX < window.innerWidth - 50) {
+    return d3.event.pageX + "px";
+  }
+  else {
+    return window.innerWidth - 50 - div.node().getBoundingClientRect().width + "px";
+  }
+};
